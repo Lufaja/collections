d144a7f87abf69c51b9549d266e5eb8381d40adf
Lukas <Lukas.v.hulst@gmail.com>
2021-09-28 10:58:44 +0200

    Nu maak ik gebruik van dictionary om de zak te vullen

---
+++ m-and-m.py
@@ -0,0 +1,18 @@
+import random
+
+#lijst met M&M kleuren
+kleuren = ["oranje", "blauw", "groen", "bruin"]
+
+#kleuren in de zak stoppen met bepaald aantal
+def genereerZak(aantal:int):
+    zak = {}
+    for i in range (int(aantal)):
+        kleur = random.choice(kleuren)
+        if kleur in zak:
+            zak[kleur] += 1
+        else:
+            zak.update({kleur : 1})
+    return zak
+
+x = input("Hoeveel M&M's wil je in de zak? ")
+print(genereerZak(x))
