e3b8c239b4fe722e1dfcfab1450f6b847bf5c09a
Lukas <Lukas.v.hulst@gmail.com>
2021-09-28 10:24:17 +0200

    MandM.py gemaakt die zak vult met kleuren

---
+++ MandM.py
@@ -0,0 +1,13 @@
+import random
+
+#lijst met M&M kleuren
+kleuren = ["oranje", "blauw", "groen", "bruin"]
+
+#kleuren in de zak stoppen met bepaald aantal
+def genereerZak(aantal:int):
+    zak = []
+    for i in range (int(aantal)):
+        zak.append(random.choice(kleuren))
+    return zak
+x = input("Hoeveel M&M's wil je in de zak? ")
+print(genereerZak(x))
